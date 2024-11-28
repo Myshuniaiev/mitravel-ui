33af404ed43dc1460fe7a836ee53b778f5290cba
Oleksii Myshuniaiev <oleksii.myshuniaiev@gmail.com>
2024-11-28 15:50:16 -0500

    creation of the greetings component

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Dashboard(props: {
   const searchParams = await props.searchParams;
 
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+    <section className="flex flex-col items-center justify-center gap-4 py-2 md:py-10 px-8">
       <Greetings />
       <SearchBar />
       <TourCards params={searchParams} />
