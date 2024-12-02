7aedb36d1b98db0c7d82db0d91c2ec48bc64d7eb
Oleksii Myshuniaiev <oleksii.myshuniaiev@gmail.com>
2024-12-01 20:20:07 -0500

    removal of the commended code

--- services/api.ts
+++ services/api.ts
@@ -19,37 +19,6 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN;
 const apiVersion = process.env.NEXT_PUBLIC_API_VERSION;
 const apiUrl = `${apiDomain}/api/${apiVersion}`;
 
-// export async function fetchTours(
-//   options: IOptions = {}
-// ): Promise<IFetchResponse<ITour[]>> {
-//   const { method = "GET", params } = options;
-
-//   try {
-//     const url = new URL(`${apiUrl}/tours`);
-
-//     if (params) {
-//       Object.keys(params).forEach((key) =>
-//         url.searchParams.append(key, params[key])
-//       );
-//     }
-
-//     const res = await fetch(url.toString(), { method });
-
-//     if (!res.ok) {
-//       const errorData = await res.json();
-
-//       throw new Error(errorData.message || "Failed to fetch tours");
-//     }
-
-//     const responseData = await res.json();
-
-//     return responseData || [];
-//   } catch (error) {
-//     console.error("Fetch Error:", error);
-//     throw new Error("An error occurred while fetching tours");
-//   }
-// }
-
 export async function request<T>(
   options: IOptions
 ): Promise<IFetchResponse<T>> {
