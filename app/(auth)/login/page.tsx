c4040fd2fb31f93d0ebcaaa47b2cf4875db12eb7
Oleksii Myshuniaiev <67588654+Myshuniaiev@users.noreply.github.com>
2024-12-30 21:22:04 -0500

    Configurations fix (#29)
    * modified package.json
    
    * removal of the type module
    
    * react/no-unknown-property for SVG attributes
    
    * disabling react/jsx-sort-props
    
    * fix unknown property 'stroke-linecap' found, use 'strokeLinecap'
    
    * added ids to input tags
    
    * Radio group  installation (#30)
    
    * installation of @nextui-org/radio
    
    * modified inputs and radio buttons

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -36,6 +36,7 @@ export default function Login() {
         <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
           <div className="flex flex-col">
             <Input
+              id="name"
               classNames={{
                 base: "-mb-[2px]",
                 inputWrapper:
@@ -50,6 +51,7 @@ export default function Login() {
               onValueChange={setName}
             />
             <Input
+              id="email"
               classNames={{
                 base: "-mb-[2px]",
                 inputWrapper:
@@ -64,6 +66,7 @@ export default function Login() {
               onValueChange={setEmail}
             />
             <Input
+              id="password"
               classNames={{
                 inputWrapper: "rounded-t-none",
               }}
@@ -81,8 +84,8 @@ export default function Login() {
                     >
                       <path
                         stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         strokeWidth="2"
                         d="M3.933 13.909A4.357 4.357 0 0 1 3 12c0-1 4-6 9-6m7.6 3.8A5.068 5.068 0 0 1 21 12c0 1-3 6-9 6-.314 0-.62-.014-.918-.04M5 19 19 5m-4 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                       />
